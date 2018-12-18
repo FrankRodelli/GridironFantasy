b48f6650f5acd316c81c3435d48b6565853ba017
frankrodelli93@gmail.com <2r46tWqBWAgs>
2018-12-18 17:26:20 -0500

    Add project files.

---
+++ FantasyGridiron/Models/Player.cs
@@ -0,0 +1,41 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace FantasyGridiron.Models
+{
+    public class Player
+    {
+        public ObjectId Id { get; set; }
+
+        [BsonElement("ESPNId")]
+        public string ESPNId { get; set; }
+
+        [BsonElement("Name")]
+        public string PlayerName { get; set; }
+
+        [BsonElement("Position")]
+        public string PlayerPosition { get; set; }
+
+        [BsonElement("Team")]
+        public string PlayerTeam { get; set; }
+
+        //Might need to change this to bson int if any errors
+        [BsonElement("Number")]
+        public int PlayerNumber { get; set; }
+
+        [BsonElement("Born")]
+        public string PlayerBOD { get; set; }
+
+        [BsonElement("Drafted")]
+        public string PlayerDrafted { get; set; }
+
+        [BsonElement("Experience")]
+        public string PlayerExperience { get; set; }
+
+        [BsonElement("College")]
+        public string PlayerCollege { get; set; }
+
+        [BsonElement("Picture")]
+        public string PlayerImage { get; set; }
+    }
+}
---
+++ FantasyGridiron/Services/PlayerService.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using FantasyGridiron.Models;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace FantasyGridiron.Services
+{
+    public class PlayerService
+    {
+        private readonly IMongoCollection<Player> _players;
+
+        public PlayerService(IConfiguration config)
+        {
+            var client = new MongoClient(config.GetConnectionString("GridironFantasyDB"));
+            var database = client.GetDatabase("GridironFantasyDB");
+            _players = database.GetCollection<Player>("Players");
+        }
+
+        public List<Player> Get()
+        {
+            return _players.Find(book => true).ToList();
+        }
+
+        public Player Get(string id)
+        {
+            var docId = new ObjectId(id);
+            return _players.Find<Player>(player => player.Id == docId).FirstOrDefault();
+        }
+
+        public Player Create(Player player)
+        {
+            _players.InsertOne(player);
+            return player;
+        }
+
+        public void Update(string id, Player playerIn)
+        {
+            var docId = new ObjectId(id);
+
+            _players.ReplaceOne(Player => Player.Id == docId, playerIn);
+        }
+
+        public void Remove(Player playerIn)
+        {
+            _players.DeleteOne(player => player.Id == playerIn.Id);
+        }
+
+        public void Remove(ObjectId id)
+        {
+            _players.DeleteOne(player => player.Id == id);
+        }
+    }
+}
