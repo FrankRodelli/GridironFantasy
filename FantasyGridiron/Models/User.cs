3b5fa8cc9dbb3a64d81d97f7425f93aa4dd138f4
frankrodelli93@gmail.com <2r46tWqBWAgs>
2018-12-18 17:48:51 -0500

    Started user model

---
+++ FantasyGridiron/Models/User.cs
@@ -0,0 +1,29 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+
+namespace FantasyGridiron.Models
+{
+    public class User
+    {
+        public ObjectId Id { get; set; }
+
+        [BsonElement("Username")]
+        public string Username { get; set; }
+
+        [BsonElement("Password")]
+        public string Password { get; set; }
+
+        [BsonElement("Email")]
+        public string Email { get; set; }
+
+        [BsonElement("FirstName")]
+        public string FirstName { get; set; }
+
+        [BsonElement("LastName")]
+        public string LastName { get; set; }
+
+        [BsonElement("Photo")]
+        public string Photo { get; set; }
+    }
+}
